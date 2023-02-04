6eae0eaf93c4751bf8b7d30634eac30b2de433a4
vano <sinkovvv1@gmail.com>
2023-02-04 18:06:53 +0200

    додання списку предметів з їх коефіціентами

--- test.js
+++ test.js
@@ -5,6 +5,7 @@ let k1= document.getElementById("K1");
 let k2= document.getElementById("K2");
 let k3= document.getElementById("K3");
 let s= document.getElementById("S");
+let q= document.getElementById("Q");
 function checkForm()
 {
    // (((parseFloat(s.value)-2)*10)+100)
@@ -14,4 +15,11 @@ function checkForm()
 
  document.getElementById("R").value=summa;
    
+}
+
+function cheakKoef(d1,d2,d3)
+{
+    k1.value=d1;
+    k2.value=d2;
+    k3.value=d3;
 }
