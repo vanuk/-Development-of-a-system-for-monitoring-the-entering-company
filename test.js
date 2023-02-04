a3af97f78b14ebb2873eede3c2b12d4e661eafa0
vano <sinkovvv1@gmail.com>
2023-02-04 12:46:10 +0200

    створення базового обрахунку

---
+++ test.js
@@ -0,0 +1,14 @@
+let a= document.getElementById("A");
+let b= document.getElementById("B");
+function checkForm()
+{
+
+
+ summa = parseInt(a.value)+parseInt(b.value);
+ document.getElementById("R").value=summa;
+   // var name11 =el.name11.value;
+   // res=name+name1;
+   // name11a
+  //  document.write(res);
+   // return false;
+}
